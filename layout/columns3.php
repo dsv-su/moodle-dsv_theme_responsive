0331ea24e1220ff283eaf818b62492d1ad5ca077
Pavel Sokolov <pavel.m.sokolov@gmail.com>
2014-01-20 14:43:16 +0400

    header css

--- layout/columns3.php
+++ layout/columns3.php
@@ -73,11 +73,13 @@ echo $OUTPUT->doctype() ?>
 <div id="page" class="container-fluid">
 
     <header id="page-header" class="clearfix">
+        <div class="dsvlogo">
+            <?php echo $html->heading; ?>
+        </div>
         <div id="page-navbar" class="clearfix">
             <div class="breadcrumb-nav"><?php echo $OUTPUT->navbar(); ?></div>
             <nav class="breadcrumb-button"><?php echo $OUTPUT->page_heading_button(); ?></nav>
         </div>
-        <?php echo $html->heading; ?>
         <div id="course-header">
             <?php echo $OUTPUT->course_header(); ?>
         </div>
