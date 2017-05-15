a3d51cdf3b572b0f18c54283793fe14bb2cc2934
Pavel Sokolov <pavel.m.sokolov@gmail.com>
2017-05-15 17:02:29 +0200

    Fix: primary button colors, pix_url, navigation tree icons

--- lib.php
+++ lib.php
@@ -167,7 +167,7 @@ function theme_dsv_theme_responsive_get_html_for_settings(renderer_base $output,
         'title' => get_string('home'),
         'class' => 'logo',
         'style' => 'background: 
-            url('.$OUTPUT->pix_url("open_courseware", "theme").') no-repeat 0 0'
+            url('.$OUTPUT->image_url("open_courseware", "theme").') no-repeat 0 0'
         ));
 
     $return->footnote = '';
