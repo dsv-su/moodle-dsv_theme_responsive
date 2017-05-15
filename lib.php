dc0629b98f10b0b7a3534bfa5746059069927335
Pavel Sokolov <pavel.m.sokolov@gmail.com>
2017-05-15 17:02:29 +0200

    Fix: primary button colors, pix_url, navigation tree icons

--- lib.php
+++ lib.php
@@ -167,7 +167,7 @@ function theme_dsv_theme_responsive_get_html_for_settings(renderer_base $output,
         'title' => get_string('home'),
         'class' => 'logo',
         'style' => 'background: 
-            url('.$OUTPUT->pix_url("natgeo-logo-". $current_language, "theme").') no-repeat 0 0'
+            url('.$OUTPUT->image_url("natgeo-logo-", "theme").') no-repeat 0 0'
         ));
 
     $return->footnote = '';
