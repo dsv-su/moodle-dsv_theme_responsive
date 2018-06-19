32bee156327270590dff10c17cbe9b7b8dc3902a
Pavel Sokolov <pavel.m.sokolov@gmail.com>
2018-06-19 11:43:03 +0300

    small fix to footer git status

--- layout/columns3.php
+++ layout/columns3.php
@@ -111,7 +111,7 @@ echo $OUTPUT->doctype() ?>
         <div id="course-footer"><?php echo $OUTPUT->course_footer(); ?></div>
         <p class="helplink"><?php //echo $OUTPUT->page_doc_link(); ?></p>
         <?php
-        echo get_string('footer', 'theme_dsv_theme_responsive');
+        echo $html->footnote;
         //echo $OUTPUT->login_info();
         //echo $OUTPUT->home_link();
         echo $OUTPUT->standard_footer_html();
