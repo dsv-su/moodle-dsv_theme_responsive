91b58132cd951691965e134cdbcdc59c44f2f992
Pavel Sokolov <pavel.m.sokolov@gmail.com>
2018-07-04 20:42:10 +0300

    Add a mockup for renderers

---
+++ classes/core_renderer.php
@@ -0,0 +1,28 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+require_once($CFG->dirroot . '/theme/bootstrapbase/renderers.php');
+
+/**
+ * Clean core renderers.
+ *
+ * @package    theme_dsv_theme_responsive
+ * @copyright  2018 Pavel Sokolov
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class theme_dsv_theme_responsive_core_renderer extends theme_bootstrapbase_core_renderer {
+    // here we can override core renderers
+}
