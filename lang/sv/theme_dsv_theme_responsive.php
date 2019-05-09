32786844922189aea3802f1254354e62e74f21c8
Pavel Sokolov <pavel.m.sokolov@gmail.com>
2019-05-09 13:15:00 +0300

    change dept name

--- lang/sv/theme_dsv_theme_responsive.php
+++ lang/sv/theme_dsv_theme_responsive.php
@@ -50,7 +50,7 @@ $string['configtitle'] = 'DSV Theme Responsive';
 $string['customcss'] = 'Custom CSS';
 $string['customcssdesc'] = 'Whatever CSS rules you add to this textarea will be reflected in every page, making for easier customization of this theme.';
 
-$string['footer'] = '<b>Institutionen för naturgeografi och kvartärgeologi</b><br>Stockholms universitet, SE-106 91 Stockholm | Telefon: <a href="callto:+468162000">08-16 20 00</a>';
+$string['footer'] = '<b>Institutionen för naturgeografi</b><br>Stockholms universitet, SE-106 91 Stockholm | Telefon: <a href="callto:+468162000">08-16 20 00</a>';
 
 $string['footnote'] = 'Footnote';
 $string['footnotedesc'] = 'Whatever you add to this textarea will be displayed in the footer throughout your Moodle site.';
